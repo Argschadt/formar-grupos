70bd9d396d0555f29d6ddcbc6f015d26434c585a
Argschadt <heitorpreigschadt@gmail.com>
2022-10-01 16:34:36 -0300

    comentarios adicionados

--- FormarGrupos.java
+++ FormarGrupos.java
@@ -1,14 +1,16 @@
 import java.util.Random;
-import java.util.Arrays;
+
 public class FormarGrupos {
-    static String[] randomize( String arr[])
+    // recebe a lista e a devolve embaralhada
+    static String[] randomize(String arr[])
     {
         int n = arr.length;
 
         Random r = new Random();
           
         for (int i = n-1; i > 0; i--) {
-              
+            
+            // j vai ser um indice menor que o atual, funcao eh exclusiva
             int j = r.nextInt(i);
               
             String temp = arr[i];
@@ -24,11 +26,14 @@ public class FormarGrupos {
          int grupo = 1;
 
          for ( int i = names.length - 1 ; i > 0 ; i=i-2){
+
+            // numero de alunos par, forma apenas duplas
             if ( names.length % 2 == 0 ) {
                 System.out.println("Grupo " + grupo + ": " + names[i] + " " + names[i-1]);
                 grupo++;
             }
             
+            // numero de alunos impar, forma um trio com os 3 ultimos alunos
             else{
                 if( i != 2) {
                     System.out.println("Grupo " + grupo + ": " + names[i] + " " + names[i-1]);
